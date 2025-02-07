8b4735d717eaa5995d3c4a7f6564514cb3085f2a
Anita O <152099173+anyta0@users.noreply.github.com>
2025-02-07 10:35:45 -0500

    Updated files to fix more issues with movie page

--- movies/views.py
+++ movies/views.py
@@ -7,16 +7,16 @@ def index(request):
         movies = Movie.objects.filter(name__icontains=search_term) #filter movies based on the search term
     else:
         movies = Movie.objects.all() #retrieve all movies if no search term is provided
+
     template_data = {}
     template_data['title'] = 'Movies'
-    template_data['movies'] = Movie.objects.all()
-    return render(request, 'movies/index.html',
-                  {'template_data': template_data})
+    template_data['movies'] = movies
+    return render(request, 'movies/index.html', {'template_data': template_data})
 
 def show(request, id):
     movie = Movie.objects.get(id=id) #retrieve a specific movie based on its id
+
     template_data = {}
     template_data['title'] = movie.name #now access movie.name as an object attribute
     template_data['movie'] = movie
-    return render(request, 'movies/show.html',
-                  {'template_data': template_data})
+    return render(request, 'movies/show.html', {'template_data': template_data})
