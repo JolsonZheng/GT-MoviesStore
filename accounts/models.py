af5c5f1c961727d27220e1c80ef0dae67e6ea36d
Will <william.zhang.d@gmail.com>
2025-02-18 09:40:06 -0500

    Add password reset functionality

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PasswordReset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
